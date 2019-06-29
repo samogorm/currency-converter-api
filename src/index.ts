747b2f9dbe0657504d5f0fe79d4385eb8ff238df
Sam <samogorm@gmail.com>
2019-06-29 08:24:14 +0100

    Set up express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("helo world");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Running....')
+});
+
+app.listen(5000);

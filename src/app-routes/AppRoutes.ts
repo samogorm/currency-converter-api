74834e004b1cad7ad1ebee3341ecb03c761d88ce
Sam <samogorm@gmail.com>
2019-06-30 12:26:29 +0100

    test route

---
+++ src/app-routes/AppRoutes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import DailyExchangeRateController from './../daily-exchange-rates/DailyExchangeRate.controller';
+
+const router = express.Router();
+
+router.route('/test').get((req, res) => {
+    let dailyExchangeRateController = new DailyExchangeRateController();
+    dailyExchangeRateController.storeDailyCurrencyCodes();
+    return res.send(`Success!`);
+});
+
+export default router;

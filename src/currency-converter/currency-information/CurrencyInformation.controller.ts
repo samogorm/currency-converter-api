552a9c4463a9c20e1fe41e4ece474c34a8855f3e
Sam <samjgorm@gmail.com>
2020-03-15 12:28:03 +0000

    Fix spacing on CurrencyInformation controller.

--- src/currency-converter/currency-information/CurrencyInformation.controller.ts
+++ src/currency-converter/currency-information/CurrencyInformation.controller.ts
@@ -3,44 +3,44 @@ import DailyExchangeRate from "../daily-exchange-rates/DailyExchangeRate.model";
 import { StaticCurrencyInformation } from "./StaticCurrencyInformation";
 
 class CurrencyInformationController {
-    /**
-     * Gets all currency information records.
-     */
-    getCurrencyInformation = async() => {
-        let currencyInformation = new CurrencyInformation();
+  /**
+   * Gets all currency information records.
+   */
+  getCurrencyInformation = async() => {
+    const currencyInformation = new CurrencyInformation();
 
-        return await currencyInformation.getAll();
-    }
+    return await currencyInformation.getAll();
+  }
 
-    /**
-     * Stores all th currency information into a db table.
-     * Uses the static array of data in './StaticCurrencyInformation.ts' to
-     * populate the documents.
-     * 
-     * It is ran only once so that we have info in the database.
-     */
-    storeCurrencyInformation = () => {
-        StaticCurrencyInformation.forEach(data => {
-            let currencyInformation = new CurrencyInformation();
+  /**
+   * Stores all th currency information into a db table.
+   * Uses the static array of data in './StaticCurrencyInformation.ts' to
+   * populate the documents.
+   * 
+   * It is ran only once so that we have info in the database.
+   */
+  storeCurrencyInformation = () => {
+    StaticCurrencyInformation.forEach(data => {
+      const currencyInformation = new CurrencyInformation();
 
-            currencyInformation.store(data);
-        });
-    }
+      currencyInformation.store(data);
+    });
+  }
 
-    getLatestExchangeRates = async(code: string) => {
-        const currencyInformation: any = new CurrencyInformation();
-        let latestCurrencyInformation = await currencyInformation.getByCode(code);
+  getLatestExchangeRates = async(code: string) => {
+    const currencyInformation: any = new CurrencyInformation();
+    const latestCurrencyInformation = await currencyInformation.getByCode(code);
 
-        const dailyExchangeRate = new DailyExchangeRate();
-        let latestExchangeRates = await dailyExchangeRate.getByBase(code);
-        
-        let data = {
-            currency_information: latestCurrencyInformation,
-            exchange_rates: latestExchangeRates
-        };
+    const dailyExchangeRate = new DailyExchangeRate();
+    const latestExchangeRates = await dailyExchangeRate.getByBase(code);
+    
+    const data = {
+      currency_information: latestCurrencyInformation,
+      exchange_rates: latestExchangeRates
+    };
 
-        return data;
-    }
+    return data;
+  }
 }
 
 export default CurrencyInformationController;

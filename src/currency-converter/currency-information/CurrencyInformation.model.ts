47aba38a57d999cd0fb0a2d6a5ab06f096339606
Sam <samjgorm@gmail.com>
2020-03-15 12:30:00 +0000

    Refactor Currency Information model.

--- src/currency-converter/currency-information/CurrencyInformation.model.ts
+++ src/currency-converter/currency-information/CurrencyInformation.model.ts
@@ -2,60 +2,48 @@ import CurrencyInformationSchema from './CurrencyInformation.schema';
 
 class CurrencyInformation {
 
-    /**
-     * This will store one record into the currency information db table.
-     * 
-     * @param {any} data the array of data to be stored.
-     * 
-     * @return {Boolean}
-     */
-    store = (data: any) => {
-        let currencyInformationSchema = new CurrencyInformationSchema();
+  /**
+   * This will store one record into the currency information db table.
+   * 
+   * @param {any} data the array of data to be stored.
+   * 
+   * @return {Boolean}
+   */
+  store = (data: any) => {
+    const currencyInformationSchema = new CurrencyInformationSchema();
 
-        currencyInformationSchema.country = data.country;
-        currencyInformationSchema.currency = data.currency;
-        currencyInformationSchema.code = data.code;
-        currencyInformationSchema.number = data.number;
-        currencyInformationSchema.archived = data.archived;
+    currencyInformationSchema.country = data.country;
+    currencyInformationSchema.currency = data.currency;
+    currencyInformationSchema.code = data.code;
+    currencyInformationSchema.number = data.number;
+    currencyInformationSchema.archived = data.archived;
 
-        return currencyInformationSchema.save(function (err) {
-            if (err) {
-                console.log(`ERROR: ${err}`)
-                return false;
-            }
+    return currencyInformationSchema.save(err => err ? false : true);
+  }
 
-            return true;
-        });
+  /**
+   * Gets all the currency information from the database.
+   */
+  getAll = async() => {
+    let currencyInformation: any = [];
+    await CurrencyInformationSchema.find()
+      .then(data => currencyInformation = data);
+    
+    return currencyInformation;
+  }
 
-    }
+  /**
+   * Gets the currency information by the code.
+   * 
+   * @param {string} code the currency code.
+   */
+  getByCode = async(code: string) => {
+    let currencyInformation: any;
+    await CurrencyInformationSchema.findOne({code: code})
+      .then(data => currencyInformation = data);
 
-    /**
-     * Gets all the currency information from the database.
-     */
-    getAll = async() => {
-        let currencyInformation: any = [];
-        await CurrencyInformationSchema.find()
-            .then(data => {
-                currencyInformation = data;
-            });
-        
-        return currencyInformation;
-    }
-
-    /**
-     * Gets the currency information by the code.
-     * 
-     * @param {string} code the currency code.
-     */
-    getByCode = async(code: string) => {
-        let currencyInformation: any;
-        await CurrencyInformationSchema.findOne({code: code})
-            .then(data => {
-                currencyInformation = data;
-            });
-
-        return currencyInformation;
-    }
+    return currencyInformation;
+  }
 }
 
 export default CurrencyInformation;

c9f7cff1c36b3d2bc8439a37f7b39cce2b6e115d
Sam <samjgorm@gmail.com>
2020-03-15 12:26:47 +0000

    Fix tab spacing on DailyExchangeRate interface.

--- src/currency-converter/daily-exchange-rates/IDailyExchangeRate.interface.ts
+++ src/currency-converter/daily-exchange-rates/IDailyExchangeRate.interface.ts
@@ -1,9 +1,9 @@
 import { Document } from 'mongoose';
 
 export interface IDailyExchangeRate extends Document {
-    date: string;
-    base_currency: string;
-    rates: object;
-    datetime_retrieved?: string;
-    archivable: boolean;
+  date: string;
+  base_currency: string;
+  rates: object;
+  datetime_retrieved?: string;
+  archivable: boolean;
 }

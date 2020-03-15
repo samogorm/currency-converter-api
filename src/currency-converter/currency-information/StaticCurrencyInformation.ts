f08e4478ac0b97778d45b443066287b0b95b7b2b
Sam <samjgorm@gmail.com>
2020-03-15 12:31:40 +0000

    Fix spacing on static currency info.

--- src/currency-converter/currency-information/StaticCurrencyInformation.ts
+++ src/currency-converter/currency-information/StaticCurrencyInformation.ts
@@ -1,227 +1,227 @@
 export const StaticCurrencyInformation = [
-    {
-        country: 'Canada',
-        currency: 'Canadian Dollar',
-        code: 'CAD',
-        number: 124,
-        country_flag: 'Canada',
-        archived: false
-    },
-    {
-        country: 'Hong Kong',
-        currency: 'Hong Kong Dollar',
-        code: 'HKD',
-        number: 344,
-        archived: false
-    },
-    {
-        country: 'Iceland',
-        currency: 'Iceland Krona',
-        code: 'HKD',
-        number: 344,
-        archived: false
-    },
-    {
-        country: 'The Philippines',
-        currency: 'Philippine Peso',
-        code: 'PHP',
-        number: 608,
-        archived: false
-    },
-    {
-        country: 'Denmark',
-        currency: 'Danish Krone',
-        code: 'DKK',
-        number: 208,
-        archived: false
-    },
-    {
-        country: 'Hungary',
-        currency: 'Forint',
-        code: 'HUF',
-        number: 348,
-        archived: false
-    },
-    {
-        country: 'Czech Republic',
-        currency: 'Czech Koruna',
-        code: 'CZK',
-        number: 203,
-        archived: false
-    },
-    {
-        country: 'Australia',
-        currency: 'Czech Koruna',
-        code: 'AUD',
-        number: 36,
-        archived: false
-    },
-    {
-        country: 'Romania',
-        currency: 'Romanian Leu',
-        code: 'RON',
-        number: 946,
-        archived: false
-    },
-    {
-        country: 'Sweden',
-        currency: 'Swedish Krona',
-        code: 'SEK',
-        number: 752,
-        archived: false
-    },
-    {
-        country: 'Indonesia',
-        currency: 'Rupiah',
-        code: 'IDR',
-        number: 360,
-        archived: false
-    },
-    {
-        country: 'India',
-        currency: 'Indian Rupee',
-        code: 'INR',
-        number: 356,
-        archived: false
-    },
-    {
-        country: 'Brazil',
-        currency: 'Brazilian Real',
-        code: 'BRL',
-        number: 986,
-        archived: false
-    },
-    {
-        country: 'Russia',
-        currency: 'Russian Ruble',
-        code: 'RUB',
-        number: 643,
-        archived: false
-    },
-    {
-        country: 'Croatia',
-        currency: 'Kuna',
-        code: 'HRK',
-        number: 191,
-        archived: false
-    },
-    {
-        country: 'Japan',
-        currency: 'Yen',
-        code: 'JPY',
-        number: 392,
-        archived: false
-    },
-    {
-        country: 'Thailand',
-        currency: 'Baht',
-        code: 'THB',
-        number: 764,
-        archived: false
-    },
-    {
-        country: 'Liechtenstein',
-        currency: 'Swiss Franc',
-        code: 'CHF',
-        number: 756,
-        archived: false
-    },
-    {
-        country: 'Singapore',
-        currency: 'Singapore Dollar',
-        code: 'SGD',
-        number: 702,
-        archived: false
-    },
-    {
-        country: 'Poland',
-        currency: 'Zloty',
-        code: 'PLN',
-        number: 985,
-        archived: false
-    },
-    {
-        country: 'Bulgaria',
-        currency: 'Bulgarian Lev',
-        code: 'BGN',
-        number: 975,
-        archived: false
-    },
-    {
-        country: 'Turkey',
-        currency: 'Turkish Lira',
-        code: 'TRY',
-        number: 949,
-        archived: false
-    },
-    {
-        country: 'China',
-        currency: 'Yuan Renminbi',
-        code: 'CNY',
-        number: 156,
-        archived: false
-    },
-    {
-        country: 'Norway',
-        currency: 'Norweigan Krone',
-        code: 'NOK',
-        number: 578,
-        archived: false
-    },
-    {
-        country: 'New Zealand',
-        currency: 'New Zealand Dollar',
-        code: 'NZD',
-        number: 554,
-        archived: false
-    },
-    {
-        country: 'South Africa',
-        currency: 'Rand',
-        code: 'ZAR',
-        number: 710,
-        archived: false
-    },
-    {
-        country: 'United States of America',
-        currency: 'United States Dollar',
-        code: 'USD',
-        number: 840,
-        archived: false
-    },
-    {
-        country: 'Mexico',
-        currency: 'Mexican Peso',
-        code: 'MXN',
-        number: 484,
-        archived: false
-    },
-    {
-        country: 'Israel',
-        currency: 'New Israeli Sheqel',
-        code: 'ILS',
-        number: 376,
-        archived: false
-    },
-    {
-        country: 'Great Britain',
-        currency: 'Great British Pound',
-        code: 'GBP',
-        number: 826,
-        archived: false
-    },
-    {
-        country: 'Korea',
-        currency: 'Won',
-        code: 'KRW',
-        number: 410,
-        archived: false
-    },
-    {
-        country: 'Europe',
-        currency: 'Euro',
-        code: 'EUR',
-        number: 978,
-        archived: false
-    }
-]
+  {
+      country: 'Canada',
+      currency: 'Canadian Dollar',
+      code: 'CAD',
+      number: 124,
+      country_flag: 'Canada',
+      archived: false
+  },
+  {
+      country: 'Hong Kong',
+      currency: 'Hong Kong Dollar',
+      code: 'HKD',
+      number: 344,
+      archived: false
+  },
+  {
+      country: 'Iceland',
+      currency: 'Iceland Krona',
+      code: 'HKD',
+      number: 344,
+      archived: false
+  },
+  {
+      country: 'The Philippines',
+      currency: 'Philippine Peso',
+      code: 'PHP',
+      number: 608,
+      archived: false
+  },
+  {
+      country: 'Denmark',
+      currency: 'Danish Krone',
+      code: 'DKK',
+      number: 208,
+      archived: false
+  },
+  {
+      country: 'Hungary',
+      currency: 'Forint',
+      code: 'HUF',
+      number: 348,
+      archived: false
+  },
+  {
+      country: 'Czech Republic',
+      currency: 'Czech Koruna',
+      code: 'CZK',
+      number: 203,
+      archived: false
+  },
+  {
+      country: 'Australia',
+      currency: 'Czech Koruna',
+      code: 'AUD',
+      number: 36,
+      archived: false
+  },
+  {
+      country: 'Romania',
+      currency: 'Romanian Leu',
+      code: 'RON',
+      number: 946,
+      archived: false
+  },
+  {
+      country: 'Sweden',
+      currency: 'Swedish Krona',
+      code: 'SEK',
+      number: 752,
+      archived: false
+  },
+  {
+      country: 'Indonesia',
+      currency: 'Rupiah',
+      code: 'IDR',
+      number: 360,
+      archived: false
+  },
+  {
+      country: 'India',
+      currency: 'Indian Rupee',
+      code: 'INR',
+      number: 356,
+      archived: false
+  },
+  {
+      country: 'Brazil',
+      currency: 'Brazilian Real',
+      code: 'BRL',
+      number: 986,
+      archived: false
+  },
+  {
+      country: 'Russia',
+      currency: 'Russian Ruble',
+      code: 'RUB',
+      number: 643,
+      archived: false
+  },
+  {
+      country: 'Croatia',
+      currency: 'Kuna',
+      code: 'HRK',
+      number: 191,
+      archived: false
+  },
+  {
+      country: 'Japan',
+      currency: 'Yen',
+      code: 'JPY',
+      number: 392,
+      archived: false
+  },
+  {
+      country: 'Thailand',
+      currency: 'Baht',
+      code: 'THB',
+      number: 764,
+      archived: false
+  },
+  {
+      country: 'Liechtenstein',
+      currency: 'Swiss Franc',
+      code: 'CHF',
+      number: 756,
+      archived: false
+  },
+  {
+      country: 'Singapore',
+      currency: 'Singapore Dollar',
+      code: 'SGD',
+      number: 702,
+      archived: false
+  },
+  {
+      country: 'Poland',
+      currency: 'Zloty',
+      code: 'PLN',
+      number: 985,
+      archived: false
+  },
+  {
+      country: 'Bulgaria',
+      currency: 'Bulgarian Lev',
+      code: 'BGN',
+      number: 975,
+      archived: false
+  },
+  {
+      country: 'Turkey',
+      currency: 'Turkish Lira',
+      code: 'TRY',
+      number: 949,
+      archived: false
+  },
+  {
+      country: 'China',
+      currency: 'Yuan Renminbi',
+      code: 'CNY',
+      number: 156,
+      archived: false
+  },
+  {
+      country: 'Norway',
+      currency: 'Norweigan Krone',
+      code: 'NOK',
+      number: 578,
+      archived: false
+  },
+  {
+      country: 'New Zealand',
+      currency: 'New Zealand Dollar',
+      code: 'NZD',
+      number: 554,
+      archived: false
+  },
+  {
+      country: 'South Africa',
+      currency: 'Rand',
+      code: 'ZAR',
+      number: 710,
+      archived: false
+  },
+  {
+      country: 'United States of America',
+      currency: 'United States Dollar',
+      code: 'USD',
+      number: 840,
+      archived: false
+  },
+  {
+      country: 'Mexico',
+      currency: 'Mexican Peso',
+      code: 'MXN',
+      number: 484,
+      archived: false
+  },
+  {
+      country: 'Israel',
+      currency: 'New Israeli Sheqel',
+      code: 'ILS',
+      number: 376,
+      archived: false
+  },
+  {
+      country: 'Great Britain',
+      currency: 'Great British Pound',
+      code: 'GBP',
+      number: 826,
+      archived: false
+  },
+  {
+      country: 'Korea',
+      currency: 'Won',
+      code: 'KRW',
+      number: 410,
+      archived: false
+  },
+  {
+      country: 'Europe',
+      currency: 'Euro',
+      code: 'EUR',
+      number: 978,
+      archived: false
+  }
+];

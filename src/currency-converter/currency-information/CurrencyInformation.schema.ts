061dcdd4042e4dab6150a7b82ae0471361fb3169
Sam <samjgorm@gmail.com>
2020-03-15 12:30:39 +0000

    Fix tab spacing on the CurrencyInformation schema

--- src/currency-converter/currency-information/CurrencyInformation.schema.ts
+++ src/currency-converter/currency-information/CurrencyInformation.schema.ts
@@ -2,15 +2,15 @@ import mongoose, {Schema} from 'mongoose';
 import { ICurrencyInformation } from './ICurrencyInformation.interface';
 
 const CurrencyInformationSchema = new Schema({
-    country: String,
-    currency: String,
-    code: String,
-    number: String,
-    archived: Boolean,
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
+  country: String,
+  currency: String,
+  code: String,
+  number: String,
+  archived: Boolean,
+  created_at: {
+      type: Date,
+      default: Date.now
+  }
 });
 
 export default mongoose.model<ICurrencyInformation>('CurrencyInformation', CurrencyInformationSchema);

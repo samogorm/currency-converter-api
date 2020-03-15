674fdac05e81c7a5a77f8ac25c6e6f67d566ba23
Sam <samjgorm@gmail.com>
2020-03-15 12:23:44 +0000

    Refactor ScheduleJob util.

--- src/utils/ScheduleJob.util.ts
+++ src/utils/ScheduleJob.util.ts
@@ -1,14 +1,14 @@
 class ScheduleJobUtil {
-    /**
-     * This will run a function every (x) milliseconds.
-     * 
-     * @param {number} minutes The number of minutes.
-     * @param {Function} jobToRun The function to run.
-     */
-    runEvery = (minutes: number = 1, jobToRun: Function) => {
-        let milliseconds = minutes * 60 * 1000;
-        setInterval(() => jobToRun(), milliseconds);
-    }
+  /**
+   * This will run a function every (x) milliseconds.
+   * 
+   * @param {number} minutes The number of minutes.
+   * @param {Function} jobToRun The function to run.
+   */
+  runEvery = (minutes: number = 1, jobToRun: Function) => {
+    const milliseconds = minutes * 60 * 1000;
+    setInterval(() => jobToRun(), milliseconds);
+  }
 }
 
 export default ScheduleJobUtil;

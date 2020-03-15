c1bf1cd83e1408ebc049776ccbdad0b5b64ce5f5
Sam <samjgorm@gmail.com>
2020-03-15 12:25:15 +0000

    Refactor DailyExchangeRate model.

--- src/currency-converter/daily-exchange-rates/DailyExchangeRate.model.ts
+++ src/currency-converter/daily-exchange-rates/DailyExchangeRate.model.ts
@@ -1,61 +1,50 @@
 import DailyExchangeRateSchema from './DailyExchangeRate.schema';
 
 class DailyExchangeRate {
+  /**
+   * Stores a currency rate to our database.
+   *
+   * @param String base The base currency.
+   */
+  store = async(exchangeRate: any) => {
+    const dailyExchangeRateSchema = new DailyExchangeRateSchema();
 
-    /**
-     * Stores a currency rate to our database.
-     *
-     * @param String base The base currency.
-     */
-    store = async(exchangeRate: any) => {
-        let dailyExchangeRateSchema = new DailyExchangeRateSchema();
+    dailyExchangeRateSchema.date = exchangeRate.date;
+    dailyExchangeRateSchema.base_currency = exchangeRate.base;
+    dailyExchangeRateSchema.rates = exchangeRate.rates;
+    dailyExchangeRateSchema.archivable = false;
 
-        dailyExchangeRateSchema.date = exchangeRate.date;
-        dailyExchangeRateSchema.base_currency = exchangeRate.base;
-        dailyExchangeRateSchema.rates = exchangeRate.rates;
-        dailyExchangeRateSchema.archivable = false;
+    return dailyExchangeRateSchema.save(err => err ? false : true);
+  }
 
-        return dailyExchangeRateSchema.save(function (err) {
-            if (err) {
-                console.log(`ERROR: ${err}`)
-                return false;
-            }
-        
-            return true;
-        });
-    }
+  /**
+   * Gets exchange rates for the base currency.
+   * 
+   * @param {String} base the base currency code.
+   */
+  getByBase = async(base: string) => {
+    let exchangeRates: any;
+    await DailyExchangeRateSchema.findOne({base_currency: base}).then(data => {
+        exchangeRates = data;
+    });
+    return exchangeRates;
+  }
 
-    /**
-     * Gets exchange rates for the base currency.
-     * 
-     * @param {String} base the base currency code.
-     */
-    getByBase = async(base: string) => {
-        let exchangeRates: any;
-        await DailyExchangeRateSchema.findOne({base_currency: base}).then(data => {
-            exchangeRates = data;
-        });
-        return exchangeRates;
-    }
-
-    /**
-     * Removes old entries from the database if they are older
-     * than yesterday.
-     * 
-     * If we have only ONE set of data for a specific day, and that 
-     * data is 3 days old or older, we will keep that last record set.
-     */
-    removeOldEntries = async() => {
-        let today = new Date();
-        let yesterday = new Date().setDate(today.getDate() - 1);
-       
-        return await DailyExchangeRateSchema.find({datetime_retrieved: {$lte: yesterday}}).remove(err => {
-            if(err) return false;
-
-            return true;
-        });
-    }
-
+  /**
+   * Removes old entries from the database if they are older
+   * than yesterday.
+   * 
+   * If we have only ONE set of data for a specific day, and that 
+   * data is 3 days old or older, we will keep that last record set.
+   */
+  removeOldEntries = async() => {
+    const today = new Date();
+    const yesterday = new Date().setDate(today.getDate() - 1);
+    
+    return await DailyExchangeRateSchema
+    .find({datetime_retrieved: {$lte: yesterday}})
+    .remove(err => err ? false : true);
+  }
 }
 
 export default DailyExchangeRate;
